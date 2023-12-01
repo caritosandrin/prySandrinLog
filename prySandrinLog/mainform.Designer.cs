42ae14e1d72668d85d7f3fca05d5f81d62745c99
caritosandrin <carolinasandrin3@gmail.com>
2023-12-01 19:22:13 -0300

    07/12/23

--- prySandrinLog/mainform.Designer.cs
+++ prySandrinLog/mainform.Designer.cs
@@ -28,14 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainform));
             this.statusPrincipal = new System.Windows.Forms.StatusStrip();
             this.lblEstadoConexion = new System.Windows.Forms.ToolStripStatusLabel();
             this.btnConectarBase = new System.Windows.Forms.Button();
             this.DataGridViewLOG = new System.Windows.Forms.DataGridView();
             this.btnTraerDatos = new System.Windows.Forms.Button();
-            this.btnRegistrar = new System.Windows.Forms.Button();
-            this.lblID = new System.Windows.Forms.Label();
-            this.txtID = new System.Windows.Forms.TextBox();
             this.statusPrincipal.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.DataGridViewLOG)).BeginInit();
             this.SuspendLayout();
@@ -44,7 +42,7 @@
             // 
             this.statusPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.lblEstadoConexion});
-            this.statusPrincipal.Location = new System.Drawing.Point(0, 532);
+            this.statusPrincipal.Location = new System.Drawing.Point(0, 412);
             this.statusPrincipal.Name = "statusPrincipal";
             this.statusPrincipal.Padding = new System.Windows.Forms.Padding(2, 0, 21, 0);
             this.statusPrincipal.Size = new System.Drawing.Size(881, 22);
@@ -91,51 +89,21 @@
             this.btnTraerDatos.UseVisualStyleBackColor = false;
             this.btnTraerDatos.Click += new System.EventHandler(this.btnMostrar_Click);
             // 
-            // btnRegistrar
-            // 
-            this.btnRegistrar.BackColor = System.Drawing.Color.Brown;
-            this.btnRegistrar.Location = new System.Drawing.Point(18, 213);
-            this.btnRegistrar.Margin = new System.Windows.Forms.Padding(4);
-            this.btnRegistrar.Name = "btnRegistrar";
-            this.btnRegistrar.Size = new System.Drawing.Size(195, 55);
-            this.btnRegistrar.TabIndex = 4;
-            this.btnRegistrar.Text = "Listar y registrar informacion";
-            this.btnRegistrar.UseVisualStyleBackColor = false;
-            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
-            // 
-            // lblID
-            // 
-            this.lblID.AutoSize = true;
-            this.lblID.Location = new System.Drawing.Point(15, 354);
-            this.lblID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblID.Name = "lblID";
-            this.lblID.Size = new System.Drawing.Size(22, 16);
-            this.lblID.TabIndex = 5;
-            this.lblID.Text = "ID";
-            // 
-            // txtID
-            // 
-            this.txtID.Location = new System.Drawing.Point(54, 348);
-            this.txtID.Name = "txtID";
-            this.txtID.Size = new System.Drawing.Size(100, 22);
-            this.txtID.TabIndex = 6;
-            // 
             // mainform
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.RosyBrown;
-            this.ClientSize = new System.Drawing.Size(881, 554);
-            this.Controls.Add(this.txtID);
-            this.Controls.Add(this.lblID);
-            this.Controls.Add(this.btnRegistrar);
+            this.ClientSize = new System.Drawing.Size(881, 434);
             this.Controls.Add(this.btnTraerDatos);
             this.Controls.Add(this.DataGridViewLOG);
             this.Controls.Add(this.btnConectarBase);
             this.Controls.Add(this.statusPrincipal);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "mainform";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FORMULARIO PRINCIPAL";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.statusPrincipal.ResumeLayout(false);
@@ -153,9 +121,6 @@
         private System.Windows.Forms.Button btnConectarBase;
         private System.Windows.Forms.DataGridView DataGridViewLOG;
         private System.Windows.Forms.Button btnTraerDatos;
-        private System.Windows.Forms.Button btnRegistrar;
-        private System.Windows.Forms.Label lblID;
-        private System.Windows.Forms.TextBox txtID;
     }
 }
 
